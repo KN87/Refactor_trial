73451ea47cfeaea4d517b8c7f0aa9545f898aadf
Keka Nandi <kn262@afsconnect1.njit.edu>
2017-12-04 13:47:28 -0500

    Namespace for db

---
+++ DatabaseId.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: keka
+ * Date: 12/4/2017
+ * Time: 1:56 AM
+ */
+
+define('connection','sql2.njit.edu');
+define('username','kn262');
+define('password','NvlYN5s5');
+define('dbname','kn262');
